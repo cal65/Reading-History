d2c594149788a2f78904f43c1be6307b6d1ad93e
Cal Lee <cal.christopheralee@gmail.com>
2021-07-19 20:46:10 -0400

    fix: passing in list as string
    Error caught by test. Needed to remove for loop that turned string into a list

--- Books/update_artifact.py
+++ Books/update_artifact.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import sys
-from google_answer import lookup_unfound
+from google_answer import append_nationalities
 from wikipedia import search_person_for_gender
 import gender_guesser.detector as gender
 
 
 def guess_gender(author):
     d = gender.Detector()
-    first_name = [name[0] if name is not None else "" for name in author.str.split(" ")]
-    gender = d.get_gender(first_name)  # this returns male, female, unknown or andy
-    if gender not in ["male", "female"]:
-        gender = search_person_for_gender(author)
-    return gender
+    names = author.split(" ")
+    first_name = names[0] if names is not None else ""
+    gender_return = d.get_gender(first_name)  # this returns male, female, unknown or andy
+    if gender_return not in ["male", "female"]:
+        gender_return = search_person_for_gender(author)
+    return gender_return
 
 
 def update_artifact(df, author_col="Author", artifact_path="authors_database.csv"):
+    df.drop_duplicates(author_col, inplace=True)
     authors_database = pd.read_csv(artifact_path)
     df_new = df[~df[author_col].isin(authors_database[author_col])]
     df_new = append_nationalities(df_new)

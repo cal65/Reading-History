7c082fd89199039886f5c5c93b42448f6b4ad9f4
Cal Lee <cal.christopheralee@gmail.com>
2021-09-11 13:57:27 -0400

    feat: adding and improving arg parse for title author sync
    Allow for limits to be added

--- Books/title_author_sync.py
+++ Books/title_author_sync.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import logging
+import argparse
 import google_answer as ga
 from scrape_goodreads import guess_gender
 
@@ -37,16 +38,33 @@ def sync(
     books_addon, authors_db, book_author_col="Author", authors_author_col="Author"
 ):
     books_addon = books_addon[pd.notnull(books_addon["nationality1"])]
-    books_addon = guess_gender(books_addon, gender_col = "gender_fixed")
+    books_addon = guess_gender(books_addon, gender_col="gender_fixed")
     common_cols = list(set(books_addon.columns).intersection(set(authors_db.columns)))
     authors_db = pd.concat([authors_db, books_addon[common_cols]])
     return authors_db
 
 
 if __name__ == "__main__":
-    books_file_path = sys.argv[1]
+    """
+    python title_author_sync.py export_goodreads.csv --limit 100
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("books_file_path", help="The path to a books csv file")
+    parser.add_argument(
+        "--limit",
+        dest="limit",
+        required=False,
+        type=int,
+        nargs=None,
+        help="The number of rows to consider from the end of books file path",
+    )
+    args = parser.parse_args()
+    books_file_path = args.books_file_path
     authors_file_path = "authors_database.csv"
     books_db = remove_nulls(books_file_path)
+    limit = int(args.limit)
+    if limit:
+        books_db = books_db.tail(limit)
     authors_db = pd.read_csv(authors_file_path)
     books_addon = lookup_nationalities(books_db, authors_db)
     authors_db = sync(books_addon, authors_db)
